ccdd75a982f4bb23ff2e15afecc141a74afb06df
Shreyansh Arora <144546852+ShreyanshArora@users.noreply.github.com>
2024-04-16 23:48:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+        
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function () {
+        
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["Software Developer", "Software Engineer", "Talented Coder"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Website Maker", "Open Source Contributer", "Freelancer", "MERN Stack Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+});
